c0117494134c0920b2a0291b1f07c82122bc7efe
Jeffery Ozoekwe-Awagu <72825145+Jeffawe@users.noreply.github.com>
2025-02-21 16:48:48 -0500

    Added page

--- src/app/editor/[page]/page.tsx
+++ src/app/editor/[page]/page.tsx
@@ -1,9 +1,11 @@
-import GridEditor from './GridEditorN';
-import { useRouter } from 'next/router';
+'use client';
+
+import GridEditor from '../GridEditorN';
+import { useParams } from 'next/navigation';
 
 export default function GridPage() {
-  const router = useRouter();
-  const id = router.query.page;
+  const params = useParams();
+  const id = params?.page;
 
   const dataID = Array.isArray(id) ? id[0] : id;
 
